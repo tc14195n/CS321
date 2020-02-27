0bc36b5c42a42446e0045f724bcd11708ee7a5cf
gitteej <tj.d.carlson@gmail.com>
2020-02-27 15:30:00 -0500

    Initial commit of local LunarLander project

---
+++ LunarLander/Assets/Scripts/spriteSpinner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteSpinner : MonoBehaviour
+{
+    float rotateSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateSpeed *= Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
+}
