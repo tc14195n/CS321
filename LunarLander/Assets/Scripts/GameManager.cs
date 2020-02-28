624cdd0be1ac24f0a6d23dd4433b538b51d25b17
gitteej <tj.d.carlson@gmail.com>
2020-02-28 16:35:51 -0500

    Added GameData and (G)(M) to start handling scene management

---
+++ LunarLander/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Text landingText;
+    int scene_index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene_index = SceneManager.GetActiveScene().buildIndex;
+        switch (scene_index)
+        {
+            case 2:
+                landingText.text = (GameData.successfulLanding ? "Successful landing!" : "Failed landing!");
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
