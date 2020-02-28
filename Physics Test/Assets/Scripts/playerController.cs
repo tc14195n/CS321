f2899bc0c09105d358ecde4e33c9798339fa180e
tc14195n <tc14195n@pace.edu>
2020-02-28 10:29:11 -0500

    Updating repo with unity projects AFTER correcting .gitignore

---
+++ Physics Test/Assets/Scripts/playerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+	Rigidbody2D rb;
+
+	Vector2 force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        force = Vector2.zero;
+        if(Input.GetKey(KeyCode.A)){
+        	force.x = -10;
+        } else if(Input.GetKey(KeyCode.D)){
+        	force.x = 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            force.y = 300;
+        }
+    }
+
+    
+    private void FixedUpdate()
+    {
+    	rb.AddForce(force);
+    	rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x,-5,5), rb.velocity.y);
+    	Debug.Log(rb.velocity.x);
+    }
+}
---
+++ SpaceGame/Assets/Scripts/meteorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float x_speed = 1;
+    float y_speed = -1;
+    float rotateSpeed = 45;
+
+    void Start()
+    {
+        rotateSpeed = Random.Range(1,4) * 60;
+        //speed = Random.Range(-5,-2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector3 pos = transform.position;
+    	pos.y += y_speed * Time.deltaTime;
+        pos.x += x_speed * Time.deltaTime;
+    	transform.position = pos;
+
+        //transform.Translate(x_speed*Time.deltaTime, y_speed*Time.deltaTime, 0);
+
+        //transform.Rotate(0, 0, rotateSpeed*Time.deltaTime); 
+    }
+}
