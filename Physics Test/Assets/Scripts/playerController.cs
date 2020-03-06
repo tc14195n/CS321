9c4c8f4f3317bfc1168502739c676a9ac639b70b
tc14195n <tc14195n@pace.edu>
2020-03-06 15:51:43 -0500

    Physics Test and Platformer
    Completed Playlist 6 videos on animation and tilemapping.  Fixed jump issue with last video.
    Platformer: Brought in animations for character and attempting to work with tilemap.  Currently have 2 palettes, one needs to go.

--- Physics Test/Assets/Scripts/playerController.cs
+++ Physics Test/Assets/Scripts/playerController.cs
@@ -6,11 +6,16 @@ public class playerController : MonoBehaviour
 {
     public LayerMask ground;
 	Rigidbody2D rb;
-
+    Animator anim;
 	Vector2 force;
+    bool facingRight = true;
+    bool jumpPending = false;
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -24,24 +29,53 @@ public class playerController : MonoBehaviour
         } else if(Input.GetKey(KeyCode.D)){
         	force.x = 10;
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector2 feet = new Vector2(transform.position.x, transform.position.y - 0.5f);
-            Vector2 dimensions = new Vector2(0.8f, 0.2f);
+            Vector2 feet = new Vector2(transform.position.x - 0.1f, transform.position.y - 0.45f);
+            Vector2 dimensions = new Vector2(1.0f, 0.2f);
             bool grounded = Physics2D.OverlapBox(feet, dimensions, 0, ground);
-            //Debug.Log(grounded);
-            
+            Debug.Log(grounded);
+
             if (grounded)
-                force.y = 300;
+                jumpPending = true;
         }
+        if(force.x < 0)
+        {
+            anim.SetBool("isWalking", true);
+            if (facingRight)
+                Flip();
+        } else if(force.x > 0)
+        {
+            anim.SetBool("isWalking", true);
+            if (facingRight == false)
+                Flip();
+        } else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
     }
 
     
     private void FixedUpdate()
-    {
+    {   
+        if(jumpPending)
+        {
+            force.y = 400;
+            jumpPending = false;
+        }
     	rb.AddForce(force);
-    	rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x,-5,5), rb.velocity.y);
-        Debug.Log(transform.position.x + ", " + transform.position.y);
+    	rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x,-5,5), Mathf.Clamp(rb.velocity.y,-5,6.5f));
+        //Debug.Log(transform.position.x + ", " + transform.position.y);
+        //Debug.Log("force: " + rb.velocity);
 
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingRight = !facingRight;
     }
 }
