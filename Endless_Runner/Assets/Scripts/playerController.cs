591658d6e9e426e7fc7a33a6e1ea80ab8f492f70
tc14195n <tj.d.carlson@gmail.com>
2020-04-24 11:57:18 -0400

    Movement and path generation
    Movement solved. Jumping needs to be fine-tuned. It's using triggers which isn't ideal. Add music and sounds effects, start menu and game over screen.

--- Endless_Runner/Assets/Scripts/playerController.cs
+++ Endless_Runner/Assets/Scripts/playerController.cs
@@ -8,19 +8,25 @@ public class playerController : MonoBehaviour
     Vector3 force;
     float z_speed, x_speed;
     float jump_force;
+    SphereCollider sphere_col;
+    [SerializeField] private LayerMask ground;
+    bool grounded;
     // Start is called before the first frame update
     //float last_built = -5;
     void Start()
     {
+        grounded = false;
+        sphere_col = GetComponent<SphereCollider>();
         player_rb = GetComponent<Rigidbody>();
-        z_speed = 50;
-        x_speed = 25;
-        jump_force = 40;
+        z_speed = 20;
+        x_speed = 15;
+        jump_force = 200;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(grounded);
         force = Vector3.zero;
         /*
          * Control scheme:
@@ -38,35 +44,68 @@ public class playerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             force.z = z_speed;
-            player_rb.AddForce(force);
+            //player_rb.AddForce(force);
         } else if (Input.GetKey(KeyCode.S))
         {
             force.z = -z_speed;
-            player_rb.AddForce(force);
+            //player_rb.AddForce(force);
         }
         if (Input.GetKey(KeyCode.A))
         {
             force.x = -x_speed;
-            player_rb.AddForce(force);
+            //player_rb.AddForce(force);
         } else if (Input.GetKey(KeyCode.D))
         {
             force.x = x_speed;
-            player_rb.AddForce(force);
+            //player_rb.AddForce(force);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && grounded)
         {
             force.y = jump_force;
-            player_rb.AddForce(force);
+            //player_rb.AddForce(force);
         }
 
     }
     private void FixedUpdate()
     {
+        player_rb.AddForce(force);
+        player_rb.velocity = new Vector3(Mathf.Clamp(player_rb.velocity.x, -4, 4), Mathf.Clamp(player_rb.velocity.y, -6, 6), Mathf.Clamp(player_rb.velocity.z, -10, 10));
         //if()
     }
+    /*
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "OOB")
             GameData.setGameOver(true);
+    }
+    */
+    private void checkGround()
+    {
+        /*
+        float extra_height = 0.3f;
+        bool grounded = Physics.BoxCast(sphere_col.bounds.center,sphere_col.bounds.size/2, Vector3.down, Quaternion.identity, extra_height, ground);
+        return grounded;
+        */
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "floor_tile")
+        {
+            grounded = true;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "floor_tile")
+        {
+            grounded = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "floor_tile")
+        {
+            grounded = false;
+        }
     }
 }
